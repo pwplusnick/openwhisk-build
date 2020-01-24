f0fe75fa997d42b78570c1870602a16f60c38a1f
Will Plusnick <pwplusni@us.ibm.com>
2020-01-23 21:50:20 -0600

    Add the python dependecies install

--- tests/test.sh
+++ tests/test.sh
@@ -6,11 +6,14 @@ else
     COMMAND=$1
 fi
 
-kubectl $COMMAND -f tasks/install-deps.yaml
-kubectl $COMMAND -f tests/taskrun-install-npm-packages.yaml
+#kubectl $COMMAND -f tasks/install-deps.yaml
+kubectl $COMMAND -f tasks/python/01-install-deps.yaml
+#kubectl $COMMAND -f tests/taskrun-install-npm-packages.yaml
+kubectl $COMMAND -f tests/taskrun-install-pip-packages.yaml
 # Get the status of taskrun
 # kubectl get taskrun.tekton.dev/taskrun-install-npm-packages -o json | jq .status.conditions[].status
 # kubectl get taskrun.tekton.dev/taskrun-install-npm-packages-without-path -o json | jq .status.conditions[].status
 
-kubectl $COMMAND -f tasks/build-archive.yaml
-kubectl $COMMAND -f tests/taskrun-build-archive.yaml
+#kubectl $COMMAND -f tasks/javascript/build-archive.yaml
+#kubectl $COMMAND -f tests/javascript/taskrun-build-archive.yaml
+
